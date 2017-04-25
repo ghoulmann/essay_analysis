0cb8d77c6039d775375e04f669bbbbdb17517e63
Rik Goldman <rikgoldman@gmail.com>
2017-04-25 12:47:03 -0400

    argparse work in progress; added some Sample output.

--- analyze.py
+++ analyze.py
@@ -9,30 +9,42 @@ import os
 import argparse
 from input import read_document as input
 
-def main(path, *string):
-    Document = input.Sample(path, string)
-    print(Document.raw_text)
-    print(Document.path)
-    print(Document.author)
-    print(Document.abs_path)
-    #print(Document.be_verb_count)
-    #print(str(Document.be_verb_ratio) + " %")
+def main(path, *author):
+    Document = input.Sample(path, author)
+    print("Base Name: " + Document.file_name)
+    print("Relative path to file: " + Document.path)
+    print("Author: " + str(Document.author))
+    print("Absolute path to file: " + Document.abs_path)
+    print("Weak Verbs to Sentences: " + str(Document.weak_verbs_to_sentences))
+    print("Weak verb count: " + str(Document.be_verb_count))
+    print("Percent passive sentences: " + str(Document.percent_passive) + " %")
+    print("Sentence Count: " + str(Document.sentence_count))
+    print("Example passive sentence from document: " + Document.passive_sentences[-1])
+    print("Example weak-verb usage: " + Document.weak_sentences_all[-2])
 
-def usage():
-    print "Requires path, 'author name'."
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze Student Writing')
-    parser.add_argument("-a", "--author", help="set writer's name", action="store_true", required=True)
-    parser.add_argument("-p", "--path", help="set path to file to analyze", action="store_true", required=True)
+    parser.add_argument("filename", help="set path to file to analyze")
+    parser.add_argument('-a', '--author', help="set writer's name", action="store", type=str, nargs='?')
+    parser.add_argument('-v', '--verbose', help="verbose output", action="store_true")
+
     args = parser.parse_args()
 
 
-    if len(sys.argv) == 3:
-        if os.path.isfile(sys.argv[1]):
-            main(sys.argv[1], str(sys.argv[2]))
-        else:
-            usage()
+    if os.path.isfile(args.filename):
+        filename = args.filename
+
+    if args.author:
+        author = str(args.author)
     else:
-        usage()
+        author = ""
+
+    if args.verbose:
+        print "Path to file: " + args.filename
+        print "Author: " + args.author
+
+
+    main(filename, author)
