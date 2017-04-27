a65f7e96efd18fd9fb1837e8c0ea95fb016330bb
Rik Goldman <rikgoldman@gmail.com>
2017-04-27 09:41:40 -0400

    start output mechanism

---
+++ output/markdown_out.py
@@ -0,0 +1,10 @@
+import codecs
+import tempfile
+
+class MarkdownReport:
+    def __init__(self):
+        self.filename = self.file_out.name
+
+    def file_out(self):
+        return tempfile.NamedTemporaryFile(mode='w+b', bufsize=-1, suffix="",
+                       prefix=template, dir=None, delete=False)
