61aed7c80e53ceee236a96492f6c04155624aaad
Rik Goldman <rikgoldman@gmail.com>
2017-04-27 11:44:12 -0400

    add tempfile_test

---
+++ output/test_tempfile.py
@@ -0,0 +1,11 @@
+import os
+import tempfile
+
+temp = tempfile.NamedTemporaryFile()
+try:
+    print 'temp:', temp
+    print 'temp.name:', temp.name
+finally:
+    # Automatically cleans up the file
+    temp.close()
+print 'Exists after close:', os.path.exists(temp.name)
